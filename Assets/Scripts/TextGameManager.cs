aaaef21e09cefa530f50fb4868846c206ca951ad
Shimemem <badlisjaden@gmail.com>
2023-07-23 23:40:31 +0800

    SubmitFinalsOutput

--- Assets/Scripts/TextGameManager.cs
+++ Assets/Scripts/TextGameManager.cs
@@ -9,7 +9,7 @@ public class TextGameManager : MonoBehaviour
     public TextMeshProUGUI gametitleTextMeshPro, storyTextMeshPro, hpTextMeshPro, staminaTextMeshPro;
     public string gameTitle, storyText;
     public int hpValue, staminaValue;
-    public GameObject Main_Menu, HP_Text, Stamina_Text, Level_1_Choices, Level_1_Story, Exitt;
+    public GameObject Main_Menu, HP_Text, Stamina_Text, Level_1_Choices, Level_1_Story, Level_2_Choices, Level_2_Story, Level_3_Choices, Exitt;
 
     // Start is called before the first frame update
     void Start()
@@ -45,27 +45,28 @@ public class TextGameManager : MonoBehaviour
     
 
     //Level 1 stuff ecksdee
-    public void GoDownstairs()
+    public void GoDown()
     {
-        storyText = "You tried to go downstairs while being drunked from waking up. You then fell down the stairs and died form a concussion.";
+        storyText = "You went downstairs towards the kitchen, grabbing something to eat and drink, you sit on the kitchen as you eat. After you were done with your snakc, you felt sleeping from being full as you pass out on the dining table.";
+        staminaValue += 9;
+
+        Level_1_Choices.SetActive(false);
+        Level_1_Story.SetActive(true);
+    }
+
+    public void GetOnPC()
+    {
+        storyText = "You still feel tired but can't go to sleep, so you went to your PC to play games through the night.";
         hpValue -= 10;
         staminaValue -= 1;
 
         Level_1_Choices.SetActive(false);
     }
 
-    public void GoUpstairs()
+    public void AMimir()
     {
-        storyText = "Upon waking up, you immediately went upstairs onto the attic. You were then jumpscared by an unknown figure you saw, making you trip and fall back down the stairs, dying from a concussion.";
-        hpValue -= 10;
-        staminaValue -= 1;
-
-        Level_1_Choices.SetActive(false);
-    }
-
-    public void SleepAgain()
-    {
-        storyText = "You went back to sleep as you were too tired to stand up and start the day.";
+        storyText = "You were too lazy to go up and just decided to fall asleep again even while feeling hungry.";
+        hpValue -= 5;
         staminaValue += 9;
 
         Level_1_Choices.SetActive(false);
@@ -75,10 +76,54 @@ public class TextGameManager : MonoBehaviour
 
     public void NextPart1()
     {
-        storyText = "You then wake up and notice that your room suddenly looks more vibrant than before, more colorful and dreamy, like you were in a dream.";
-        Level_1_Story.SetActive(false) ;
+        storyText = "You wake up to the feeling of really soft feeling under you, squinting your eyes from how bright it became. Upon scratching your eyes and the vision becomes clearer, you notice that what it looks like you're above the clouds.";
+        Level_1_Story.SetActive(false);
+        Level_2_Choices.SetActive(true);
     }
 
+    public void Panik()
+    {
+        storyText = "You panic from the utter shock of being what it seems another world. You wake up from the dream";
+        Level_2_Choices.SetActive(false);
+
+        hpValue = 0;
+        staminaValue = 0;
+    }
+
+    public void Amazables()
+    {
+        storyText = "You look around in awe at what you're seeing, being in a dreamlike setting high above the clouds. You see things you recall and have seen from your past dreams and scenarios you imagined on your slumber";
+        Level_2_Choices.SetActive(false);
+        Level_2_Story.SetActive(true);
+    }
+
+    public void Lv2Kwento()
+    {
+        storyText = "You then see dark monsters running at you, upon them approaching, a colorful sword appeared and spawned on your hand.";
+
+        Level_2_Story.SetActive(false);
+        Level_3_Choices.SetActive(true);
+    }
+
+    public void RunTakbo()
+    {
+        storyText = "You took the safe route and try to run away from them. Moving around as you imagine yourself as an RPG Characted in a game you play. As you were running away from the monsters, you suddenly woke up, looking at the ceiling in your room upon being woken up by your phone's alarm.";
+
+        hpValue = 0;
+        staminaValue = 5;
+
+        Level_3_Choices.SetActive(false);
+    }
+
+    public void FendOff()
+    {
+        storyText = "You stood your ground and fought, did your best to fight them off, doing tricks and moves as you imagine yourself from a RPG Game you played. But as time passes fending off all by yourself, you get overwhelemed by the amound of them, awaking from your dream upon being defeated.";
+
+        hpValue = 0;
+        staminaValue = 0;
+
+        Level_3_Choices.SetActive(false);
+    }
 
     public void ExitGame()
     {
